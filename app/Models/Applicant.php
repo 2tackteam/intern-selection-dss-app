a1221d59fc9d52f55420affba434db8fc3c9014b
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-21 17:01:18 +0700

    feat : migrations & models

---
+++ app/Models/Applicant.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Applicant extends Model
+{
+    use HasFactory;
+
+    protected $table = 'applicants';
+    protected $guarded = ['id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function educations(): HasMany
+    {
+        return $this->hasMany(Education::class);
+    }
+
+    public function documents(): HasMany{
+        return $this->hasMany(Document::class);
+    }
+
+    public function scores(): HasMany
+    {
+        return $this->hasMany(Score::class);
+    }
+}
---
+++ app/Models/Score.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Score extends Model
+{
+    use HasFactory;
+
+    protected $table = 'scores';
+    protected $guarded = ['id'];
+
+    public function applicant(): BelongsTo
+    {
+        return $this->belongsTo(Applicant::class);
+    }
+
+    public function criteria(): BelongsTo
+    {
+        return $this->belongsTo(Criteria::class);
+    }
+}
