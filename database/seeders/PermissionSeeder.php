5bdb278dbeb84a163f59dd60033ef2a61f41cdf1
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-21 13:59:28 +0700

    setup Laravel Permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+
+        $permissions = [
+            'view-menu dashboard',
+            'view-menu manage-applicant',
+            'view-menu applicant',
+            'view-menu profile',
+
+            'view applicants',
+            'create applicants',
+            'update applicants',
+            'delete applicants',
+
+            'update profile',
+            'update password',
+            'delete account'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => "$permission"]);
+        }
+
+
+        Role::create(['name' => 'admin'])->givePermissionTo($permissions);
+        Role::create(['name' => 'user'])->syncPermissions([
+            'view-menu dashboard',
+            'view-menu applicant',
+            'view-menu profile',
+
+            'view applicants',
+            'create applicants',
+
+            'update profile',
+            'update password',
+            'delete account'
+        ]);
+    }
+}
