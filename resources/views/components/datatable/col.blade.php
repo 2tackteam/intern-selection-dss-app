5448efec4ddd27719507dd24b867acf71057e9de
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-23 04:57:27 +0700

    Update col.blade.php

--- resources/views/components/datatable/col.blade.php
+++ resources/views/components/datatable/col.blade.php
@@ -1,6 +1,6 @@
 @props(['value'])
 
-<td {!! $attributes->merge(['class' => 'px-4 py-3']) !!}>
+<td {!! $attributes->merge(['class' => 'px-4 py-3 text-sm']) !!}>
     @if(isset($value))
         {{ $value }}
     @else
