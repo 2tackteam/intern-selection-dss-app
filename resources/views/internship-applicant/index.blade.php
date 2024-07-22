f80be3826599eab6b00da6f4064f62afe41d5bc9
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-23 03:26:52 +0700

    Update index.blade.php

--- resources/views/internship-applicant/index.blade.php
+++ resources/views/internship-applicant/index.blade.php
@@ -14,11 +14,15 @@
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="contain-inline-size">
 
-                    <x-primary-button>{{__('internship-applicant.buttons.selection')}}</x-primary-button>
+                    <x-primary-button class="mb-4">
+                        <i class="fas fa-filter mr-2"></i>
+                        {{__('internship-applicant.buttons.selection')}}
+                    </x-primary-button>
 
                     <x-datatable :id="'dtApplicants'" :collection="$data['applicants']">
                         <x-slot:thead>
                             <x-datatable.row isHeader>
+                                <x-datatable.col :value="'#'"/>
                                 <x-datatable.col :value="__('internship-applicant.tables.headers.full_name')"/>
                                 <x-datatable.col :value="__('internship-applicant.tables.headers.date_of_birth')"/>
                                 <x-datatable.col :value="__('internship-applicant.tables.headers.place_of_birth')"/>
@@ -31,13 +35,17 @@
                             @forelse($data['applicants'] as $applicant)
                                 @if($applicant instanceof \App\Models\Application)
                                     <x-datatable.row>
+                                        <x-datatable.col :value="$loop->iteration"/>
                                         <x-datatable.col :value="$applicant->full_name"/>
                                         <x-datatable.col :value="$applicant->birth_place"/>
                                         <x-datatable.col :value="$applicant->birth_date->translatedFormat('d F Y')"/>
                                         <x-datatable.col :value="$applicant->gender"/>
                                         <x-datatable.col :value="$applicant->education?->education_level"/>
                                         <x-datatable.col>
-                                            <x-primary-button>{{__('internship-applicant.buttons.detail')}}</x-primary-button>
+                                            <x-link-button href="/">
+                                                <i class="far fa-eye mr-2"></i>
+                                                {{__('internship-applicant.buttons.detail')}}
+                                            </x-link-button>
                                         </x-datatable.col>
                                     </x-datatable.row>
                                 @endif
@@ -45,6 +53,7 @@
                             @endforelse
                         </x-slot:tbody>
                     </x-datatable>
+
                 </div>
             </div>
         </div>
