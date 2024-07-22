07802399fbd4f59ae2a05ce4ec01e2754a078c31
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-22 19:35:09 +0700

    Update toggle-theme.blade.php

--- resources/views/components/toggle-theme.blade.php
+++ resources/views/components/toggle-theme.blade.php
@@ -15,13 +15,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js"
             integrity="sha512-6sSYJqDreZRZGkJ3b+YfdhB3MzmuP9R7X1QZ6g5aIXhRvR1Y/N/P47jmnkENm7YL3oqsmI6AK+V6AD99uWDnIw=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        let isDarkMode = false
-        let $html = $('html')
-        let $toggleButton = $('.toggle-button')
-        let $toggleIcon = $('.toggle-icon')
+        var $html = $('html')
+        var $toggleButton = $('.toggle-button')
+        var $toggleIcon = $('.toggle-icon')
 
 
         if (localStorage.getItem('darkMode') === 'true') {
