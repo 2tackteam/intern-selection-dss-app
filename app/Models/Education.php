1bae3d5ec2f0a4ae47fbc6b92e6d799904872a11
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-22 19:34:41 +0700

    fix: migration & model

--- app/Models/Education.php
+++ app/Models/Education.php
@@ -7,12 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
- * 
+ * @property int $id
+ * @property int $applicant_id
+ * @property string $education_level
+ * @property string $institution_name Nama Instansi / Pendidikan
+ * @property string $major Jurusan / Program Studi
+ * @property string $start_year Tahun Mulai Ajaran / Pendidikan
+ * @property string $end_year Tahun Lulus Ajaran / Pendidikan
+ * @property string $gpa IPK atau nilai rata-rata
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Applicant $applicant
  *
- * @property-read \App\Models\Applicant|null $applicant
  * @method static \Illuminate\Database\Eloquent\Builder|Education newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Education newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Education query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereApplicantId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereEducationLevel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereEndYear($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereGpa($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereInstitutionName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereMajor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereStartYear($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Education whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class Education extends Model
--- database/migrations/2024_07_21_164136_create_educations_table.php
+++ database/migrations/2024_07_21_164136_create_educations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('education', function (Blueprint $table) {
+        Schema::create('educations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('applicant_id')->constrained('applicants')->cascadeOnDelete();
             $table->enum('education_level', ['SMA/SMK', 'D1', 'D2', 'D3', 'D4', 'S1']);
