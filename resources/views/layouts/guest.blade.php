e3d0ca21e9017201c4bbe6591cf3e5959019e691
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-23 19:36:51 +0700

    fix missing jquery

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -34,6 +34,8 @@
             {{ $slot }}
         </div>
     </div>
-@stack('scripts')
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    @stack('scripts')
 </body>
 </html>
