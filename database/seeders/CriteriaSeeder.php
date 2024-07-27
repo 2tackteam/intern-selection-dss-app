d8e86c7e0eb0c0b4cca6a8b1a2621353cef45422
Alvin Setiawan <alvin.set25@gmail.com>
2024-07-27 10:09:44 +0700

    Update CriteriaSeeder.php

--- database/seeders/CriteriaSeeder.php
+++ database/seeders/CriteriaSeeder.php
@@ -14,27 +14,27 @@ class CriteriaSeeder extends Seeder
     {
         $criteria1 = Criteria::create([
             'name' => 'Nilai / GPA',
-            'weight' => 1,
+            'weight' => 5,
         ]);
         $criteria1->subCriterias()->createMany([
-            ['criteria_id' => $criteria1->id, 'name' => '<30', 'weight' => 1],
-            ['criteria_id' => $criteria1->id, 'name' => '30-70', 'weight' => 2],
-            ['criteria_id' => $criteria1->id, 'name' => '>70', 'weight' => 7],
+            ['criteria_id' => $criteria1->id, 'name' => '<30', 'weight' => 1, 'min_value' => 0, 'max_value' => 29.99],
+            ['criteria_id' => $criteria1->id, 'name' => '30-70', 'weight' => 2, 'min_value' => 30, 'max_value' => 70],
+            ['criteria_id' => $criteria1->id, 'name' => '>70', 'weight' => 7, 'min_value' => 70.01, 'max_value' => 100],
         ]);
 
         $criteria2 = Criteria::create([
             'name' => 'Jurusan',
-            'weight' => 2,
+            'weight' => 1,
         ]);
         $criteria2->subCriterias()->createMany([
-            ['criteria_id' => $criteria2->id, 'name' => 'A - E', 'weight' => 1],
-            ['criteria_id' => $criteria2->id, 'name' => 'F - I', 'weight' => 3],
-            ['criteria_id' => $criteria2->id, 'name' => 'J - Z', 'weight' => 5],
+            ['criteria_id' => $criteria2->id, 'name' => 'Administrasi Perkantoran, Pengembangan Masyarakat Islam, etc', 'weight' => 1],
+            ['criteria_id' => $criteria2->id, 'name' => 'Manajemen Informatika', 'weight' => 4],
+            ['criteria_id' => $criteria2->id, 'name' => 'Administrasi Publik, Pembangunan Ekonomi dan Pemberdayaan Masyarakat', 'weight' => 5],
         ]);
 
         $criteria3 = Criteria::create([
             'name' => 'Tingkat Pendidikan',
-            'weight' => 5,
+            'weight' => 2,
         ]);
         $criteria3->subCriterias()->createMany([
             ['criteria_id' => $criteria3->id, 'name' => 'SMA/SMK', 'weight' => 1],
